f7067aeb2daaab417fa73cc77ade3be6ec0a4ef4
zxcty54 <nitesh.skyhigh@gmail.com>
2026-07-25 21:53:48 +0530

    Update math_text.dart

--- flutter_app/lib/widgets/math_text.dart
+++ flutter_app/lib/widgets/math_text.dart
@@ -11,10 +11,31 @@ class MathFormattedText extends StatelessWidget {
     this.textStyle,
   });
 
+  // 🎯 HTML FORMATTING CLEANER (Fixes <b>, </b>, <br/> raw tags)
+  static String _cleanHtmlFormatting(String rawInput) {
+    if (rawInput.isEmpty) return "";
+    String cleaned = rawInput;
+
+    // 1. <br>, <br/>, <br > ko clean line break (\n) me badlein
+    cleaned = cleaned.replaceAll(RegExp(r'<br\s*/?>', caseSensitive: false), '\n');
+
+    // 2. Sirf specific HTML tags (<b>, </b>, <i>, </i>, <span>, <div>, <p>) ko hatayein
+    // Note: Isse Physics/Maths ke '<' aur '>' (jaise x < 5) comparison signs SAFE rahenge!
+    cleaned = cleaned.replaceAll(
+      RegExp(r'</?(b|i|u|strong|em|p|div|span|font)[^>]*>', caseSensitive: false), 
+      ''
+    );
+
+    return cleaned;
+  }
+
   @override
   Widget build(BuildContext context) {
-    if (!text.contains(r'\') && !text.contains(r'$')) {
-      return Text(text, style: textStyle);
+    // 🧹 Pehle HTML Tags Safai Karein
+    final String cleanText = _cleanHtmlFormatting(text);
+
+    if (!cleanText.contains(r'\') && !cleanText.contains(r'$')) {
+      return Text(cleanText, style: textStyle);
     }
 
     final List<InlineSpan> spans = [];
@@ -22,9 +43,9 @@ class MathFormattedText extends StatelessWidget {
     
     int lastMatchEnd = 0;
 
-    for (final RegExpMatch match in mathRegExp.allMatches(text)) {
+    for (final RegExpMatch match in mathRegExp.allMatches(cleanText)) {
       if (match.start > lastMatchEnd) {
-        spans.add(TextSpan(text: text.substring(lastMatchEnd, match.start), style: textStyle));
+        spans.add(TextSpan(text: cleanText.substring(lastMatchEnd, match.start), style: textStyle));
       }
 
       String mathExpr = match.group(1) ?? match.group(0) ?? '';
@@ -47,8 +68,8 @@ class MathFormattedText extends StatelessWidget {
       lastMatchEnd = match.end;
     }
 
-    if (lastMatchEnd < text.length) {
-      spans.add(TextSpan(text: text.substring(lastMatchEnd), style: textStyle));
+    if (lastMatchEnd < cleanText.length) {
+      spans.add(TextSpan(text: cleanText.substring(lastMatchEnd), style: textStyle));
     }
 
     return RichText(
