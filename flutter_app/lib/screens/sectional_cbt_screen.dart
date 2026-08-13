97bfac01db3ba7097bbe4c873e79a40c0c799b60
zxcty54 <nitesh.skyhigh@gmail.com>
2026-08-13 23:31:20 +0530

    Update sectional_cbt_screen.dart

--- flutter_app/lib/screens/sectional_cbt_screen.dart
+++ flutter_app/lib/screens/sectional_cbt_screen.dart
@@ -2,7 +2,6 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
-
 class SectionalCbtListScreen extends StatefulWidget {
   final bool isDarkMode;
   const SectionalCbtListScreen({super.key, required this.isDarkMode});
