11043e33e39e00765702746873b15ee0832585dd
zxcty54 <nitesh.skyhigh@gmail.com>
2026-09-24 10:55:07 +0530

    Update home_screen.dart

--- flutter_app/lib/screens/home_screen.dart
+++ flutter_app/lib/screens/home_screen.dart
@@ -25,7 +25,7 @@ import 'tabs/revision_tab.dart';
 import 'tabs/sectional_tab.dart';
 
 // 🛠️ Aspirant Tools Imports
-import '../widgets/exam_photo_resizer_screen.dart';
+
 import '../widgets/exam_doc_merger_screen.dart';
 
 class HomeScreen extends StatefulWidget {
